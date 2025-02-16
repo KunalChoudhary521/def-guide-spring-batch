bd9bb1c6b13a570f94b3e4893091eeccd9b3fc41
KunalChoudhary521 <kunal.choudhary@alumni.utoronto.ca>
2025-02-16 18:35:01 -0500

    Chapter02: Update to Spring Batch v5
    Spring Boot v3.4.1
    Maven 3.6.3

--- Chapter02/src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
+++ Chapter02/src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
@@ -2,45 +2,31 @@ package io.spring.batch.helloworld;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@EnableBatchProcessing
 @SpringBootApplication
 public class HelloWorldApplication {
 
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
-
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
-
 	@Bean
-	public Step step() {
-		return this.stepBuilderFactory.get("step1")
-				.tasklet(new Tasklet() {
-					@Override
-					public RepeatStatus execute(StepContribution contribution,
-							ChunkContext chunkContext) {
-						System.out.println("Hello, World!");
-						return RepeatStatus.FINISHED;
-					}
-				}).build();
+	public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+		return new StepBuilder("step1", jobRepository)
+				.tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+                    return RepeatStatus.FINISHED;
+                }, platformTransactionManager).build();
 	}
 
 	@Bean
-	public Job job() {
-		return this.jobBuilderFactory.get("job")
-				.start(step())
+	public Job job(JobRepository jobRepository, Step step) {
+		return new JobBuilder("job", jobRepository)
+				.start(step)
 				.build();
 	}
 
