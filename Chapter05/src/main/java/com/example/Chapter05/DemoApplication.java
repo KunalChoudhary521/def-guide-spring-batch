d271f3770e0cfef94087a6fa51585f23efa0621e
KunalChoudhary521 <kunal.choudhary@alumni.utoronto.ca>
2025-02-18 19:34:35 -0500

    Chapter05: Update to Spring Batch v5
    Spring Boot v3.4.1
    Maven 3.6.3

--- Chapter05/src/main/java/com/example/Chapter05/DemoApplication.java
+++ Chapter05/src/main/java/com/example/Chapter05/DemoApplication.java
@@ -5,24 +5,19 @@ import com.example.Chapter05.batch.ExploringTasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@EnableBatchProcessing
 @SpringBootApplication
 public class DemoApplication {
-
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
-
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
 
 	@Autowired
 	private JobExplorer jobExplorer;
@@ -33,16 +28,16 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public Step explorerStep() {
-		return this.stepBuilderFactory.get("explorerStep")
-				.tasklet(explorerTasklet())
+	public Step explorerStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+		return new StepBuilder("explorerStep", jobRepository)
+				.tasklet(explorerTasklet(), platformTransactionManager)
 				.build();
 	}
 
 	@Bean
-	public Job explorerJob() {
-		return this.jobBuilderFactory.get("explorerJob")
-				.start(explorerStep())
+	public Job explorerJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+		return new JobBuilder("explorerJob", jobRepository)
+				.start(explorerStep(jobRepository, platformTransactionManager))
 				.build();
 	}
 
